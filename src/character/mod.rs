f99bebb47ca3a6118203ccc614277dca0c53134e
Sharmiko <sharmiko123@gmail.com>
2022-05-19 22:24:08 +0400

    add actions and running

--- src/character/mod.rs
+++ src/character/mod.rs
@@ -1,4 +1,5 @@
 use std::path::Path;
+use std::cell::{RefCell, RefMut};
 
 use ggez::{Context, GameResult};
 use ggez::event::{EventHandler, KeyCode};
@@ -7,7 +8,25 @@ use ggez::graphics::{self, *};
 
 use glam::Vec2;
 
-use crate::consts::{CHAR_WIDTH};
+use crate::consts::{CHAR_WIDTH, RUN_SPEED};
+
+
+type Action = RefCell<MultiStageAnimation>;
+
+
+pub struct MultiStageAnimation {
+    animation: Animation,
+    performing: bool
+}
+
+impl MultiStageAnimation {
+    pub fn new(ctx: &mut Context, image_path: &str) -> Action {
+        RefCell::new(Self {
+            animation: Animation::new(ctx, image_path),
+            performing: false
+        })
+    }
+} 
 
 
 pub struct Animation {
@@ -61,12 +80,25 @@ impl CharacterState {
 pub struct Character {
     pub default: Animation,
     pub run: Animation,
+    pub jump: Action,
     x: f32,
     y: f32,
     state: CharacterState
 }
 
 impl Character {
+
+    pub fn perform_action(&self, ctx: &mut Context, mut action: RefMut<'_, MultiStageAnimation>) {
+        if !action.performing {
+            action.performing = true;
+        } else if action.animation.current + 1 == action.animation.total {
+            action.performing = false;
+        }
+
+        let src_x = action.animation.next_x();
+        let params = self.param(src_x, 0f32, action.animation.width, 1.);
+        action.animation.image.draw(ctx, params);
+    }
 
     pub fn param(&self, src_x: f32, src_y: f32, w: f32, h: f32) -> graphics::DrawParam{
         let mut params = graphics::DrawParam::default()
@@ -96,6 +128,15 @@ impl Character {
             self.state.is_flipped = false;
             self.x -= CHAR_WIDTH / 2.;
         }
+
+        self.x += RUN_SPEED;
+        let (width, _) = graphics::size(ctx);
+        self.x = if self.x + CHAR_WIDTH / 2. <= width {
+            self.x
+        } else {
+            width - CHAR_WIDTH / 2.
+        };
+
         self.run.image.draw(ctx, params);
     }
 
@@ -106,6 +147,14 @@ impl Character {
             self.state.is_flipped = true;
             self.x += CHAR_WIDTH / 2.;
         }
+
+        self.x -= RUN_SPEED;
+        self.x = if self.x - CHAR_WIDTH / 2. >= 0. {
+            self.x
+        } else {
+            CHAR_WIDTH / 2.
+        };
+
         self.run.image.draw(ctx, params);
     }
 
@@ -115,8 +164,14 @@ impl Character {
         self.default.image.draw(ctx, params);
     }
 
+    fn perform_jump(&mut self, ctx: &mut Context) {
+        self.perform_action(ctx, self.jump.borrow_mut());
+    }
+
     pub fn draw(&mut self, ctx: &mut Context)  {
-        if keyboard::is_key_pressed(ctx, KeyCode::D) {
+        if self.jump.borrow_mut().performing || keyboard::is_key_pressed(ctx, KeyCode::Space) {
+            self.perform_jump(ctx);
+        } else if keyboard::is_key_pressed(ctx, KeyCode::D) {
             self.run_right(ctx);
         } else if keyboard::is_key_pressed(ctx, KeyCode::A) {
             self.run_left(ctx);
@@ -134,6 +189,7 @@ impl Punk {
         Character {
             default: Animation::new(_ctx, "/Punk_idle.png"),
             run: Animation::new(_ctx, "/Punk_run.png"),
+            jump: MultiStageAnimation::new(_ctx, "/Punk_jump.png"),
             x: 100.,
             y: 100.,
             state: CharacterState::default()
