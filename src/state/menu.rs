989da4796b60772864540818318ad3c648113562
Sharmiko <sharmiko123@gmail.com>
2022-06-05 16:27:57 +0400

    refactor menu state to use custom rectangle

--- src/state/menu.rs
+++ src/state/menu.rs
@@ -4,23 +4,24 @@ use ggez::Context;
 use ggez::graphics::{self};
 use ggez::input::mouse::{self};
 
+use crate::base::CustomRect;
 use crate::state::{State, AllStates};
 
 
 pub struct MenuState {
-    rect: graphics::Rect
+    rect: CustomRect
 }
 
 impl MenuState {
     pub fn new(_ctx: &mut Context) ->  Self {
         let (w, h) = graphics::size(_ctx);
         Self {
-            rect: graphics::Rect{
-                x: w / 2. - 300. / 2.,
-                y: h / 2. - 60. / 2.,
-                w: 300.,
-                h: 60.
-            }
+            rect: CustomRect::new(
+                w / 2. - 300. / 2.,
+                h / 2. - 60. / 2.,
+                300.,
+                60.
+            )
         }
     }
 }
@@ -33,34 +34,21 @@ impl State for MenuState {
     fn exit(&self, _ctx: &mut Context, _current_state: &RefCell<AllStates>) {}
 
     fn draw(&mut self, ctx: &mut Context, _current_state: &RefCell<AllStates>) {
-        let mesh = graphics::MeshBuilder::new()
-            .rectangle(
-                graphics::DrawMode::stroke(3.), 
-                graphics::Rect {
-                    x: 0.,
-                    y: 0.,
-                    w: self.rect.w,
-                    h: self.rect.h
-                },
-                graphics::Color::BLUE
-            ).unwrap().build(ctx).unwrap();
-            
-
+        self.rect.draw(ctx);
         let draw_params = graphics::DrawParam::new()
-            .dest(glam::Vec2::new(self.rect.x, self.rect.y));
+            .dest(glam::Vec2::new(self.rect.fields.x, self.rect.fields.y));
 
         let text = graphics::Text::new("Play");
         graphics::draw(ctx, &text, draw_params.color(graphics::Color::RED)).unwrap();
-    
-        graphics::draw(ctx, &mesh, draw_params).unwrap();
-
     }
 
     fn update(&mut self, _ctx: &mut Context, current_state: &RefCell<AllStates>) {
         let point = mouse::position(_ctx);
         let (mouse_x, mouse_y) = (point.x, point.y);
-        if mouse_x > self.rect.x && mouse_x < self.rect.x + self.rect.w &&
-            mouse_y > self.rect.y && mouse_y < self.rect.y + self.rect.h
+        if mouse_x > self.rect.fields.x && 
+           mouse_x < self.rect.fields.x + self.rect.fields.w &&
+           mouse_y > self.rect.fields.y && 
+           mouse_y < self.rect.fields.y + self.rect.fields.h
         {
             if mouse::button_pressed(_ctx, mouse::MouseButton::Left) {
                 current_state.replace(AllStates::Play);
