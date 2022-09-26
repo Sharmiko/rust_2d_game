88b30fe7e74648f0c26f5de087090553347f5dd4
Sharmiko <sharmiko123@gmail.com>
2022-09-26 23:52:34 +0400

    update consts

--- src/consts.rs
+++ src/consts.rs
@@ -1,14 +1,7 @@
 // Character Animation Configuration
-pub const CHAR_WIDTH: f32 = 144.;
 pub const CHAR_SCALE_FACTOR: f32 = 3.;
+pub const CHAR_WIDTH: f32 = 48. *  CHAR_SCALE_FACTOR;
+pub const CHAR_HEIGHT: f32 = 48. * CHAR_SCALE_FACTOR;
 pub const RUN_SPEED: f32 = 15.;
 pub const JUMP_SPEED_DY: f32 = 10.;
 pub const JUMP_SPEED_DX: f32 = 15.;
-
-// Resource path configuration
-
-pub const PUNK_DIR: &str = "/chars/punk";
-pub const BIKER_DIR: &str = "/chars/biker";
-pub const CYBORG_DIR: &str = "/chars/cyborg";
-
-pub const PARK_DAY_BACKGROUND_DIR: &str = "/tiles/park/background/day";
