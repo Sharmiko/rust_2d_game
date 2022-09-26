28fa8762167fd099d798ec0e9eded31abbc223a6
Sharmiko <sharmiko123@gmail.com>
2022-09-26 23:53:38 +0400

    add resources for managing assets

---
+++ src/resources.rs
@@ -0,0 +1,16 @@
+pub mod chars {
+    pub const PUNK: &str = "/chars/punk";
+    pub const BIKER: &str = "/chars/biker";
+    pub const CYBORG: &str = "/chars/cyborg";
+}
+
+pub mod enemies {
+    pub const DIR: &str = "/enemies";
+}
+
+
+pub mod background {
+    pub const PARK_DAY_BACKGROUND: &str = "/tiles/park/background/day";
+    pub const PARK_NIGHT_BACKGROUND: &str = "/tiles/park/background/night";
+    pub const INDUSTRIAL_BACKGROUND: &str = "/tiles/industrial/background";
+}
