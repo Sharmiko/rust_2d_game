124bcbf318f5a9f8bd18fa086356cfbde23b1400
Sharmiko <sharmiko123@gmail.com>
2022-09-26 23:49:15 +0400

    rename CharacterAnimation enum to Animation enum

--- src/character/chars.rs
+++ src/character/chars.rs
@@ -1,9 +1,10 @@
 use ggez::Context;
 
-use crate::character::{Character, CharacterAnimation};
+use crate::character::Character;
+use crate::base::human::Animation;
 use crate::animation::SpriteAnimation;
 use crate::utils::join_paths;
-use crate::consts::{PUNK_DIR, BIKER_DIR, CYBORG_DIR};
+use crate::resources::chars;
 
 
 pub struct Punk;
@@ -11,11 +12,11 @@ pub struct Punk;
 impl Punk {
     pub fn new(_ctx: &mut Context) -> Character {
         let mut punk = Character::default(_ctx);
-        punk.insert_animation(CharacterAnimation::Idle, SpriteAnimation::new(_ctx, &join_paths(&PUNK_DIR, "Punk_idle.png")));
-        punk.insert_animation(CharacterAnimation::Run, SpriteAnimation::new(_ctx, &join_paths(&PUNK_DIR, "Punk_run.png")));
-        punk.insert_animation(CharacterAnimation::Jump,  SpriteAnimation::new(_ctx, &join_paths(&PUNK_DIR, "Punk_jump.png")));
-        punk.insert_animation(CharacterAnimation::Attack, SpriteAnimation::new(_ctx, &join_paths(&PUNK_DIR, "Punk_attack1.png")));
-        punk.insert_animation(CharacterAnimation::DoubleJump, SpriteAnimation::new(_ctx, &join_paths(&PUNK_DIR, "Punk_doublejump.png")));
+        punk.entity.insert_animation(Animation::Idle, SpriteAnimation::new(_ctx, &join_paths(&chars::PUNK, "Punk_idle.png")));
+        punk.entity.insert_animation(Animation::Run, SpriteAnimation::new(_ctx, &join_paths(&chars::PUNK, "Punk_run.png")));
+        punk.entity.insert_animation(Animation::Jump,  SpriteAnimation::new(_ctx, &join_paths(&chars::PUNK, "Punk_jump.png")));
+        punk.entity.insert_animation(Animation::Attack, SpriteAnimation::new(_ctx, &join_paths(&chars::PUNK, "Punk_attack1.png")));
+        punk.entity.insert_animation(Animation::DoubleJump, SpriteAnimation::new(_ctx, &join_paths(&chars::PUNK, "Punk_doublejump.png")));
 
         return punk;
     }
@@ -27,11 +28,11 @@ pub struct Biker;
 impl Biker {
     pub fn new(_ctx: &mut Context) -> Character {
         let mut biker = Character::default(_ctx);
-        biker.insert_animation(CharacterAnimation::Idle, SpriteAnimation::new(_ctx, &join_paths(&BIKER_DIR, "Biker_idle.png")));
-        biker.insert_animation(CharacterAnimation::Run, SpriteAnimation::new(_ctx, &join_paths(&BIKER_DIR, "Biker_run.png")));
-        biker.insert_animation(CharacterAnimation::Jump,  SpriteAnimation::new(_ctx, &join_paths(&BIKER_DIR, "Biker_jump.png")));
-        biker.insert_animation(CharacterAnimation::Attack, SpriteAnimation::new(_ctx, &join_paths(&BIKER_DIR, "Biker_attack.png")));
-        biker.insert_animation(CharacterAnimation::DoubleJump, SpriteAnimation::new(_ctx, &join_paths(&BIKER_DIR, "Biker_doublejump.png")));
+        biker.entity.insert_animation(Animation::Idle, SpriteAnimation::new(_ctx, &join_paths(&chars::BIKER, "Biker_idle.png")));
+        biker.entity.insert_animation(Animation::Run, SpriteAnimation::new(_ctx, &join_paths(&chars::BIKER, "Biker_run.png")));
+        biker.entity.insert_animation(Animation::Jump,  SpriteAnimation::new(_ctx, &join_paths(&chars::BIKER, "Biker_jump.png")));
+        biker.entity.insert_animation(Animation::Attack, SpriteAnimation::new(_ctx, &join_paths(&chars::BIKER, "Biker_attack.png")));
+        biker.entity.insert_animation(Animation::DoubleJump, SpriteAnimation::new(_ctx, &join_paths(&chars::BIKER, "Biker_doublejump.png")));
 
         return biker;
     }
@@ -43,11 +44,11 @@ pub struct Cyborg;
 impl Cyborg {
     pub fn new(_ctx: &mut Context) -> Character {
         let mut cyborg = Character::default(_ctx);
-        cyborg.insert_animation(CharacterAnimation::Idle, SpriteAnimation::new(_ctx, &join_paths(&CYBORG_DIR, "Cyborg_idle.png")));
-        cyborg.insert_animation(CharacterAnimation::Run, SpriteAnimation::new(_ctx, &join_paths(&CYBORG_DIR, "Cyborg_run.png")));
-        cyborg.insert_animation(CharacterAnimation::Jump,  SpriteAnimation::new(_ctx, &join_paths(&CYBORG_DIR, "Cyborg_jump.png")));
-        cyborg.insert_animation(CharacterAnimation::Attack, SpriteAnimation::new(_ctx, &join_paths(&CYBORG_DIR, "Cyborg_attack.png")));
-        cyborg.insert_animation(CharacterAnimation::DoubleJump, SpriteAnimation::new(_ctx, &join_paths(&CYBORG_DIR, "Cyborg_doublejump.png")));
+        cyborg.entity.insert_animation(Animation::Idle, SpriteAnimation::new(_ctx, &join_paths(&chars::CYBORG, "Cyborg_idle.png")));
+        cyborg.entity.insert_animation(Animation::Run, SpriteAnimation::new(_ctx, &join_paths(&chars::CYBORG, "Cyborg_run.png")));
+        cyborg.entity.insert_animation(Animation::Jump,  SpriteAnimation::new(_ctx, &join_paths(&chars::CYBORG, "Cyborg_jump.png")));
+        cyborg.entity.insert_animation(Animation::Attack, SpriteAnimation::new(_ctx, &join_paths(&chars::CYBORG, "Cyborg_attack.png")));
+        cyborg.entity.insert_animation(Animation::DoubleJump, SpriteAnimation::new(_ctx, &join_paths(&chars::CYBORG, "Cyborg_doublejump.png")));
 
         return cyborg;
     }
