572494ec308b68079493a29eda0e28462b63de56
Sharmiko <sharmiko123@gmail.com>
2022-10-06 19:50:51 +0400

    update consts

--- src/consts.rs
+++ src/consts.rs
@@ -3,5 +3,10 @@ pub const CHAR_SCALE_FACTOR: f32 = 3.;
 pub const CHAR_WIDTH: f32 = 48. *  CHAR_SCALE_FACTOR;
 pub const CHAR_HEIGHT: f32 = 48. * CHAR_SCALE_FACTOR;
 pub const RUN_SPEED: f32 = 15.;
+pub const WALK_SPEED: f32 = 6.;
 pub const JUMP_SPEED_DY: f32 = 10.;
 pub const JUMP_SPEED_DX: f32 = 15.;
+
+
+// Application Configuration
+pub const LEVELS_DIR: &str = "./levels";
